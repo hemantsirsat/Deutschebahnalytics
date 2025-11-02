6a7936c398edef45cb7c241bef348dccbee220c6
Hemant Sirsat <h.sirsat@tu-braunschweig.de>
2025-11-02 22:29:32 +0100

    staging: arrival and departure delay

--- calculate_delay/models/staging/stg_timetables.sql
+++ calculate_delay/models/staging/stg_timetables.sql
@@ -16,3 +16,5 @@ select
         else null
     end as departure_delay
 from {{ source('raw', 'raw_timetable') }}
+where (actual_arrival_time is not null and planned_arrival_time is not null)
+   or (actual_departure_time is not null and planned_departure_time is not null)
