1c17fa68a7a27ab6821c860b6fc88284737764da
Hemant Sirsat <h.sirsat@tu-braunschweig.de>
2025-11-02 22:17:42 +0100

    staging: arrival and departure delay

--- calculate_delay/models/staging/stg_timetables.sql
+++ calculate_delay/models/staging/stg_timetables.sql
@@ -5,5 +5,14 @@
 -- stg_timetables: compute delay and copy all raw columns
 select
     *,
-    (actual_time - scheduled_time) as delay
-from {{ source('raw', 'raw_timetables') }} WHERE (actual_time IS NOT NULL AND scheduled_time IS NOT NULL)
+    case 
+        when actual_arrival_time is not null and planned_arrival_time is not null 
+        then actual_arrival_time - planned_arrival_time
+        else null
+    end as arrival_delay,
+    case 
+        when actual_departure_time is not null and planned_departure_time is not null
+        then actual_departure_time - planned_departure_time
+        else null
+    end as departure_delay
+from {{ source('raw', 'raw_timetable') }}
