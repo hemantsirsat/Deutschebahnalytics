0dad8f7223f06aab59b122f0a35e14ef96c9e4b1
Hemant Sirsat <h.sirsat@tu-braunschweig.de>
2025-11-03 16:32:42 +0100

    average arrival-departure delay by hours and number of delays by hour

---
+++ streamlit_app.py
@@ -0,0 +1,93 @@
+import os
+import streamlit as st
+import pandas as pd
+import psycopg
+import plotly.express as px
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DBT_HOST=os.getenv("DBT_HOST")
+DBT_USER=os.getenv("DBT_USER")
+DBT_PASSWORD=os.getenv("DBT_PASSWORD")
+DBT_DBNAME=os.getenv("DBT_DBNAME")
+
+# -----------------------------
+# Database connection
+# -----------------------------
+@st.cache_data(ttl=600)
+def load_data():
+    conn = psycopg.connect(
+        host=DBT_HOST,
+        dbname=DBT_DBNAME,
+        user=DBT_USER,
+        password=DBT_PASSWORD,
+        port=5432
+    )
+    query = "SELECT * FROM fct_train_delay_summary"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
+
+# -----------------------------
+# Load data
+# -----------------------------
+st.title("Deutsche Bahn Train Delay Dashboard")
+df = load_data()
+
+# Show raw data
+st.subheader("Raw Hourly Delay Data")
+st.dataframe(df)
+
+# -----------------------------
+# Average Delays Plot
+# -----------------------------
+st.subheader("Average Delays per Hour (minutes)")
+
+fig_avg_delay = px.bar(
+    df,
+    x="hour_of_day",
+    y=["avg_arrival_delay_min", "avg_departure_delay_min"],
+    barmode="group",
+    labels={
+        "hour_of_day": "Hour of Day",
+        "value": "Average Delay (min)",
+        "variable": "Delay Type",
+        "avg_arrival_delay_min": "Average Arrival Delay (minutes)",
+        "avg_departure_delay_min": "Average Departure Delay (minutes)"
+    },
+    title="Average Arrival & Departure Delays by Hour"
+)
+
+fig_avg_delay.update_layout(
+    xaxis=dict(
+        tickmode="linear",
+        dtick=1
+    )
+)
+
+st.plotly_chart(fig_avg_delay, use_container_width=True)
+
+# -----------------------------
+# Delay Counts Plot
+# -----------------------------
+st.subheader("Number of Delays per Hour")
+
+fig_count = px.bar(
+    df,
+    x="hour_of_day",
+    y=["total_delays"],
+    barmode="group",
+    labels={"hour_of_day": "Hour of Day", "value": "Count", "variable": "Delay Type"},
+    title="Total Delays by Hour"
+)
+
+# Force all x-axis labels to show
+fig_count.update_layout(
+    xaxis=dict(
+        tickmode="linear",
+        dtick=1
+    )
+)
+
+st.plotly_chart(fig_count, use_container_width=True)
