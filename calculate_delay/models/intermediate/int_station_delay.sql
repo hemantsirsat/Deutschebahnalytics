3d9ed8abbb474058067f11ba8066031c9778e330
Hemant Sirsat <h.sirsat@tu-braunschweig.de>
2025-11-03 23:10:36 +0100

    station name, arrival/dept delay, delay count for intermediate table

--- calculate_delay/models/intermediate/int_station_delay.sql
+++ calculate_delay/models/intermediate/int_station_delay.sql
@@ -1,31 +1,12 @@
-{{ config(
-    materialized='table'
-) }}
-
-WITH stg AS (
-    SELECT
-        eva_number,
-        arrival_delay,
-        departure_delay
-    FROM {{ ref("stg_timetables") }}
-),
-
-station_info AS (
-    SELECT
-        eva_number,
-        name AS station_name
-    FROM {{ source("raw", "raw_stations") }}
-)
+{{ config(materialized='table') }}
 
 SELECT
     s.eva_number,
-    si.station_name,
-    ROUND(AVG(EXTRACT(EPOCH FROM s.arrival_delay) / 60.0), 2) AS avg_arrival_delay_min,
-    ROUND(AVG(EXTRACT(EPOCH FROM s.departure_delay) / 60.0), 2) AS avg_departure_delay_min,
-    COUNT(*) AS total_delays
-FROM stg s
-LEFT JOIN station_info si
+    si.name AS station_name,
+    s.planned_arrival_time,
+    s.planned_departure_time,
+    s.arrival_delay,
+    s.departure_delay
+FROM {{ ref("stg_timetables") }} s
+LEFT JOIN {{ source("raw", "raw_stations") }} si
     ON CAST(s.eva_number AS BIGINT) = si.eva_number
-WHERE s.arrival_delay IS NOT NULL OR s.departure_delay IS NOT NULL
-GROUP BY s.eva_number, si.station_name
-ORDER BY s.eva_number
