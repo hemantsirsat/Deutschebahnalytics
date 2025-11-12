a110892aff361cab60dea039e61eb767a7feedfe
Hemant Sirsat <h.sirsat@tu-braunschweig.de>
2025-11-12 19:06:00 +0100

    stg now materalized as views

--- calculate_delay/models/staging/stg_timetables.sql
+++ calculate_delay/models/staging/stg_timetables.sql
@@ -1,5 +1,5 @@
 {{ config(
-    materialized='table'
+    materialized='view'
 ) }}
 
 -- stg_timetables: compute delay and copy all raw columns
@@ -8,12 +8,12 @@ select
     case 
         when actual_arrival_time is not null and planned_arrival_time is not null 
         then actual_arrival_time - planned_arrival_time
-        else null
+        else interval '0 seconds'
     end as arrival_delay,
     case 
         when actual_departure_time is not null and planned_departure_time is not null
         then actual_departure_time - planned_departure_time
-        else null
+        else interval '0 seconds'
     end as departure_delay
 from {{ source('raw', 'raw_timetable') }}
 where (actual_arrival_time is not null and planned_arrival_time is not null)
